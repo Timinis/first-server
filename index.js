46589d9dd9832089296096fe9a6544778e829c5c
timinis <litimc3@gmail.com>
2019-10-16 20:44:25 -0700

    updated server file

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ app.get('/hello', (request, response) => {
   response.send('hello there');
 });
 
+app.get('/object', (request, response) => {
+  const object = { foo: 'bar', baz: 'code' };
+  const sendingJSON = JSON.stringify(object);
+  response.send(sendingJSON);
+});
+
 app.listen(3000, () => {
   console.log('server is running on port 3000');
 });
