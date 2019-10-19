38df043d8e42c8690227566eb57d79b9037b37f7
timinis <litimc3@gmail.com>
2019-10-19 11:37:25 -0700

    Query params and post

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+
+let array = ['apple', 'banana', 'cat', 'dog', 'car'];
+let contactInfo = [
+  { name: 'tim', phone: '123' },
+  { name: 'ali', phone: '456' },
+  { name: 'mustaf', phone: '789' },
+  { name: 'ahmed', phone: '001' },
+  { name: 'samson', phone: '002' }
+];
 
 app.get('/hello', (request, response) => {
   response.send('hello there');
@@ -9,6 +19,38 @@ app.get('/object', (request, response) => {
   const object = { foo: 'bar', baz: 'code' };
   const sendingJSON = JSON.stringify(object);
   response.send(sendingJSON);
+});
+
+app.get('/array', (request, response) => {
+  if (request.query.item) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === request.query.item) {
+        response.send(array[i]);
+      }
+    }
+    response.send('cannot find item');
+  }
+
+  if (!request.query.item) {
+    response.send(array);
+  }
+});
+
+app.get('/phonebook', (request, response) => {
+  if (request.query.person) {
+    for (let i = 0; i < contactInfo.length; i++) {
+      if (contactInfo[i].name === request.query.person) {
+        return response.send(contactInfo[i].phone);
+      }
+    }
+    return response.send(`cannot find person`);
+  }
+  return response.send('please enter a person query parameter');
+});
+
+app.post('/addtoarray', (request, response) => {
+  array.push(request.body.item);
+  response.send(array);
 });
 
 app.listen(3000, () => {
